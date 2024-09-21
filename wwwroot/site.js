83b53cf968de21c236105b9897f46fef1e807ddc
hectordani03 <hectordaniel010203@gmail.com>
2024-09-20 18:32:08 -0600

    Agregando primer mapa - BingMaps

---
+++ wwwroot/site.js
@@ -0,0 +1,8 @@
+﻿window.initMap = function () {
+    var map = new Microsoft.Maps.Map('#myMap', {
+        credentials: 'Ao46xYbAAZCcZ02OaPnLjKJ8MnaQcSFdiGCEL3cfmvqXPlz9CRTL8_KfWW2L0Ke8',
+        center: new Microsoft.Maps.Location(47.6149, -122.1941),
+        mapTypeId: Microsoft.Maps.MapTypeId.road,
+        zoom: 10
+    });
+}
